81e7cb49ed72ecd55e745ad7d911e8320c3cd903
AkshitMehta2005 <akshitmehta494@gmail.com>
2026-02-17 16:54:27 +0530

    data added

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -60,7 +60,7 @@ export function Navigation() {
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}
           >
-            Visit Blog
+           Strategic Notes
           </motion.a>
 
           {/* Mobile menu button */}
